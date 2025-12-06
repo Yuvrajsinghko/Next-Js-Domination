8b758c8a90230d0ac754253057aea777881fd525
Yuvrajsinghko <yuvrajprince19@gmail.com>
2025-12-06 14:36:25 +0530

    added new routes

---
+++ first-app/src/app/products/[prodictsId]/page.tsx
@@ -0,0 +1,5 @@
+export default function ProductDetails({params}:{
+    params:Promise<{productId:string}>
+}) {
+  return <h1>product details</h1>;
+}
---
+++ first-app/src/app/products/page.tsx
@@ -0,0 +1,10 @@
+export default function Products() {
+  return <>
+  <ul>
+    <li>first product</li>
+    <li>second product</li>
+    <li>thirdproduct</li>
+  </ul>
+  
+  </>;
+}
