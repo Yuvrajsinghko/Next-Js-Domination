cfd3c52b378caee911c7ae7b24a59a01e053c55c
Yuvrajsinghko <yuvrajprince19@gmail.com>
2025-12-11 15:08:47 +0530

    modified project folder

---
+++ my-app/src/app/(auth)/login/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <div>
+        <h2>Please Login to see the content</h2>
+    </div>
+  )
+}
+
+export default Login
--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -1,18 +1,9 @@
-"use client"
-import { useRouter } from "next/navigation";
-import React from "react";
+import React from 'react'
 
 const Home = () => {
-  const ro=useRouter()
-  const nt = (pag:string)=>{
-    ro.push(`${pag}`)
-  }
   return (
-    <div>
-      <h1>Welcome to home page</h1>
-      <button onClick={()=>nt("page1")}>For main content</button>
-    </div>
-  );
-};
+    <div>Home</div>
+  )
+}
 
-export default Home;
+export default Home
