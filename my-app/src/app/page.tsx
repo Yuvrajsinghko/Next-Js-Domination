62f155415c2f73f2ee295c5a2f768ab398ced8c1
Yuvrajsinghko <yuvrajprince19@gmail.com>
2025-12-11 12:00:01 +0530

    modified some files

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -1,15 +1,16 @@
-"use client";
-import Link from "next/link";
-import React from "react";
+"use client"
 import { useRouter } from "next/navigation";
+import React from "react";
 
 const Home = () => {
-  const router= useRouter();
-  console.log(router);
-  
+  const ro=useRouter()
+  const nt = (pag:string)=>{
+    ro.push(`${pag}`)
+  }
   return (
     <div>
       <h1>Welcome to home page</h1>
+      <button onClick={()=>nt("page1")}>For main content</button>
     </div>
   );
 };
