7173b32a865095c9d3ba78ddc6984826ad011e5d
Yuvrajsinghko <yuvrajprince19@gmail.com>
2025-12-16 11:11:53 +0530

    updated dynamic routes folder

--- dynamic-routes/src/app/products/[...slug]/page.tsx
+++ dynamic-routes/src/app/products/[...slug]/page.tsx
@@ -1,17 +1,17 @@
 import React from "react";
 
-const Prod = async ({ params }: { params: { slug: string } }) => {
+const Prod = async ({ params }: { params: { slug: string[] } }) => {
   const { slug } = await params;
   console.log(slug);
   return (
     <div>
       <h1>Dynamic ROot Catching all the routes </h1>
-      {/* {slug.map((idx,p)=>{
-        <ul>
-            <li key={idx}>{p}</li>
+      {slug.map((p,idx)=>(
+        <ul key={idx}>
+            <li >{p}</li>
         </ul>
 
-      })} */}
+      ))}
     </div>
   );
 };
