c5f76b1053d24f799c89b7da9b6534a0de567abd
Yuvrajsinghko <yuvrajprince19@gmail.com>
2025-12-26 16:04:02 +0530

    added new project folder prize maniac

---
+++ prize-maniac/app/page.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <main>
+        <header>
+          <div>
+            
+          </div>
+        </header>
+      </main>
+    </div>
+  );
+};
+
+export default Home;
